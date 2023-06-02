31f0ade89434902900146ec067dc04a0e4cae95b
Zz <572497921@qq.com>
2023-06-02 13:18:41 +0800

    config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,8 +3,8 @@ import { defineConfig } from "vitepress"
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Arturia",
-  description: "/Avalon/",
-  base: Avalon,
+  description: "Avalon",
+  base: "/Avalon/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
